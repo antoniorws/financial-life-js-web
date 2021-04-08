8254b86e9f1ed3b01f84a03870b74c5d1a344d11
Antonio Serra <antonioserra@chinasystems.com>
2021-04-08 14:38:39 -0300

    Ajustando configurações

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -1,23 +1,15 @@
 // BANCO DE TESTE
 var config = {
-        apiKey: "AIzaSyABZE0FoVh80SOS2rcLAmbY1BKW9vprk8E",
-        authDomain: "tutorial-4722e.firebaseapp.com",
-        databaseURL: "https://tutorial-4722e-default-rtdb.firebaseio.com",
-        projectId: "tutorial-4722e",
-        storageBucket: "tutorial-4722e.appspot.com",
-        messagingSenderId: "404861731343",
-        appId: "1:404861731343:web:82f42c6c87fe4b5364997a",
-        measurementId: "G-XVE03THVMJ"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
 };
 firebase.initializeApp(config);
 var firestore = firebase.firestore();
 
 //BANCO DE PRODUÇÃO
-/*
-var config = {
-    
-    
-};
-firebase.initializeApp(config);
-var firestore = firebase.firestore();
-*/
